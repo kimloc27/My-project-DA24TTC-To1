3e747b1f9cd003b24ad02f78c89de675e5aeb850
Loc <kimlyphuocloc2702@gmail.com>
2026-03-31 08:30:45 +0700

    Upload

---
+++ js/lib.js
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html>
+<html lang="vi">
+      <head>
+	<title>Thiết kế Web</title>
+	<meta charset="utf-8">
+      </head>
+      <body>
+	<ul>
+		<li>
+			<a href="html/page1.html"> Thông tin </a>
+		</li>
+        </ul>
+
+      </body>
+</html>
